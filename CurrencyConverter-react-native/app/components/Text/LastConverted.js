393814eedd63c1e10aca4c51e23cce025d6a5074
ekas <ekaspreet93.singh@gmail.com>
2019-06-25 00:14:01 +0530

    Added Styled Text Component

---
+++ CurrencyConverter-react-native/app/components/Text/LastConverted.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Text } from 'react-native';
+import moment from 'moment';
+import styles from './styles';
+
+const LastConverted = ({
+  date, base, quote, conversionRate,
+}) => (
+
+  <Text style={styles.smallText}>
+    1
+    {' '}
+    {base}
+    {' '}
+    =
+    {' '}
+    {conversionRate}
+    {' '}
+    {quote}
+    {' '}
+    as of
+    {' '}
+    { moment(date).format('MMMM D, YYYY') }
+  </Text>
+);
+
+LastConverted.propTypes = {
+  date: PropTypes.object,
+  base: PropTypes.string,
+  quote: PropTypes.string,
+  conversionRate: PropTypes.number,
+};
+
+export default LastConverted;
--- CurrencyConverter-react-native/app/screens/Home.js
+++ CurrencyConverter-react-native/app/screens/Home.js
@@ -6,11 +6,14 @@ import { Container } from '../components/Container';
 import { Logo } from '../components/Logo';
 import { InputWithButton } from '../components/TextInput';
 import { ClearButton } from '../components/Buttons';
+import { LastConverted } from '../components/Text';
 
 const TEMP_BASE_CURRENCY = 'USD';
 const TEMP_QUOTE_CURRENCY = 'GBP';
 const TEMP_BASE_PRICE = '100';
 const TEMP_QUOTE_PRICE = '79.74';
+const TEMP_CONVERSION_RATE = 0.7974;
+const TEMP_CONVERSION_DATE = new Date();
 
 class Home extends Component {
   handleChangeText = (text) => {
@@ -46,6 +49,12 @@ class Home extends Component {
           buttonText={TEMP_QUOTE_CURRENCY}
           onPress={this.handlePressQuoteCurrency}
           value={TEMP_QUOTE_PRICE}
+        />
+        <LastConverted
+          date={TEMP_CONVERSION_DATE}
+          quote={TEMP_QUOTE_CURRENCY}
+          base={TEMP_BASE_CURRENCY}
+          conversionRate={TEMP_CONVERSION_RATE}
         />
         <ClearButton
           text="Reverse Currency"
