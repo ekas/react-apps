e769a7b07c63e93819f5e003d9294a98b8a21e21
ekas <ekaspreet93.singh@gmail.com>
2019-07-08 21:25:56 +0530

    Improved UI for Image Gallery

--- ImageGallery-react-native/App.js
+++ ImageGallery-react-native/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
-import { Image, ScrollView, StyleSheet, Text, Button } from 'react-native';
+import { Image, ScrollView, StyleSheet, Text, Button, Dimensions, View } from 'react-native';
 import { Constants, Permissions,ImagePicker } from 'expo';
+
+const imageWidth = Dimensions.get('window').width;
 
 export default class App extends Component {
   state = { photoUri: [] };
@@ -9,14 +11,14 @@ export default class App extends Component {
     let { photoUri } = this.state;
     return (
       <ScrollView style={styles.container}>
-        <Button styles={styles.paragraph}
+        <Button styles={styles.imagePicker}
           title="Pick an image from camera roll"
           onPress={this._pickImage}
         />
         {photoUri.length != 0 ? (
           this._renderPhotos(photoUri)
         ) : (
-          <Text style={styles.paragraph}>No Images</Text>
+          <Text style={styles.text}>No Images</Text>
         )}
       </ScrollView>
     );
@@ -41,11 +43,13 @@ export default class App extends Component {
   _renderPhotos(photoUri) {
     let images = [];
     photoUri.map((uri, index) => images.push(
-      <Image key={index}
-        source={{ uri: uri }}
-        resizeMode="contain"
-        style={{ height: 100, width: 100, resizeMode: 'contain' }}
-      />
+      <View style={ styles.imageContainer } key={index}>
+        <Image
+          source={{ uri: uri }}
+          resizeMode="contain"
+          style={ styles.galleryImg }
+        />
+      </View>
     ));
     return images;
   }
@@ -59,11 +63,31 @@ const styles = StyleSheet.create({
     backgroundColor: '#ecf0f1',
     alignContent: 'center',
   },
-  paragraph: {
+  imagePicker: {
     margin: 24,
     fontSize: 18,
     fontWeight: 'bold',
     textAlign: 'center',
     color: '#34495e',
   },
+  text: {
+    margin: 24,
+    fontSize: 18,
+    fontWeight: 'bold',
+    textAlign: 'center',
+    color: '#34495e',
+  },
+  galleryImg: {
+    height: 150, 
+    width: imageWidth - 40, 
+    resizeMode: 'cover',
+  },
+  imageContainer: {
+    marginVertical: 10 ,
+    marginHorizontal: 5,
+    padding: 10,
+    borderRadius: 4,
+    borderWidth: 5,
+    borderColor: '#000',
+  }
 });
